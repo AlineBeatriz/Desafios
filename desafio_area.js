0ac265fd5d6b7491f3ea88e8bd3d9b4816c63df7
Aline Beatriz <80787118+AlineBeatriz@users.noreply.github.com>
2024-01-29 17:10:25 -0300

    Desafio
    Desafio de um curso sobre JavaScript.

---
+++ desafio_area.js
@@ -0,0 +1,14 @@
+//Desafio: Calcular a área de uma circunferência:
+    //Formula: (A = π r²) PI * raio * raio
+    //usar string M2 para representar metros-quadrados
+    //PI = 3.141592 (valor constante)
+
+let desafio = "Calcular a área de uma circunferência: "
+let raio = 10;
+const PI = 3.141592;
+
+    let areaCirc = PI * raio * raio;
+
+console.log(desafio + " (A = π r²)");
+console.log("Resultado: " + areaCirc + "m2");
+console.log(Math.PI);
